b43bdfea074c40f0374bffacccc0e9c23bab7062
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2021-01-23 13:38:27 +0200

    Added Route for path that doesn't exist. Project finished =)!! Some css styling issues still changing.

--- blog-application/src/App.js
+++ blog-application/src/App.js
@@ -3,26 +3,30 @@ import Home from './Home';
 import Create from './Create';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
     <Router>
-    <div className="App">
+      <div className="App">
       <NavBar />
-      <div className="content">
-         <Switch>
-           <Route exact path="/">
-             <Home />
-           </Route>
-           <Route path="/create">
-             <Create />
-           </Route>
-           <Route path="/blogs/:id">
-             <BlogDetails />
-           </Route>
-         </Switch>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
     </Router>
   );
 }
