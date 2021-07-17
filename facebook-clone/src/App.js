dfb408c806b04ca13a2715b75f52c219b9c95727
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2021-07-17 20:15:07 +0300

    Initial commit for new project creating Facebook-clone.

---
+++ facebook-clone/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+      <h1>Lets build the Facebook clone!!</h1>
+    </div>
+  );
+}
+
+export default App;
