9b20bacc7fa9d9f5659cdf24913a875cce07093f
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2021-07-18 02:53:19 +0300

    Finished Header component with css styling.

---
+++ facebook-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDfnvaZBvqhvy0zI2bCnCjFQZBZymzCwPA",
+    authDomain: "facebook-clone-cd09f.firebaseapp.com",
+    projectId: "facebook-clone-cd09f",
+    storageBucket: "facebook-clone-cd09f.appspot.com",
+    messagingSenderId: "871407449456",
+    appId: "1:871407449456:web:bc93db5551f59d8c25cbb5",
+    measurementId: "G-GQ5VDP6WDZ"
+  };
