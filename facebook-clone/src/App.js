b23afa342c83604518356ae8014752e695cab0bb
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2021-07-19 01:29:06 +0300

    Added Widgets in application and moved individual components inside components folder.

--- facebook-clone/src/App.js
+++ facebook-clone/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import Feed from './Feed';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import Feed from './components/Feed/Feed';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import Widgets from './components/Widgets/Widgets';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <div className="app__body">
         <Sidebar />
         <Feed />  
-          {/* Widgets */}
+        <Widgets />
       </div>
     </div>
   );
