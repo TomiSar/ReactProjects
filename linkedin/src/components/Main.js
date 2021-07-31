201aa1b54da0cfb715498011a38a249dc3718c60
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2021-07-31 03:26:38 +0300

    Created Home screen section structure from Leftside, Rightside and Main components. Styling added and completed for all the sections.

--- linkedin/src/components/Main.js
+++ linkedin/src/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Main = (props) => {
-    return (
-        <h1>Main</h1>
-    )
-}
+  return <Container>Main</Container>;
+};
+
+const Container = styled.div`
+  grid-area: main;
+`;
 
 export default Main;
